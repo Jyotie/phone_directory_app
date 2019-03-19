a6eef850875c9924afe27bb975266849b7d3b539
Jyotie <Jyotie@users.noreply.github.com>
2019-03-19 22:36:57 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,193 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PureComponent, Fragment } from "react";
+// import {render} from 'react-dom'
+import { Link, Switch, Route, withRouter }from "react-router-dom";
 
-class App extends Component {
-  render() {
+// import logo from './logo.svg';
+import './App.css'; 
+
+/*Header*/
+const Header = props => {
+  return <header className="header">{props.children}</header>;
+};
+/*Contact list*/
+const Contacts = props => {
+  const contactList = props.list.map((contact, id) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <tr key={id}>
+        <td>{contact.name}</td>
+        <td>{contact.phone}</td>
+        <td>
+          <button onClick={() => props.onDelete(contact.id)} className="btn btn-danger">DELETE</button>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <table className="table table-dark">
+      <thead>
+		<tr>
+		  <th scope="col">NAME</th>
+		  <th scope="col">PHONE</th>
+		  <th scope="col"></th>
+		</tr>
+	  </thead>
+        {contactList}
+    </table>
+  );
+};
+
+/*Add contact component & event*/
+class AddContact extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      phone: "",
+      error: false,
+      errorMessage: ""
+    };
+  }
+  onChangeName = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+  onChangePhone = event => {
+    this.setState({
+      phone: event.target.value
+    });
+  };
+  addContact = event => {
+    event.preventDefault();
+    const phone = this.state.phone;
+    if (isNaN(phone) || phone.length !== 10) {
+      this.setState({
+        error: true,
+        errorMessage: "Enter a valid phone no."
+      });
+    } else {
+      this.props.addContact({
+        name: this.state.name,
+        phone: this.state.phone
+      });
+    }
+  };
+  render() {
+    let errorMessage;
+    if (this.state.error) {
+      errorMessage = this.state.errorMessage;
+    }
+    return (
+      <Fragment>
+        <Header>ADD A SUBSCRIBER</Header>
+        <div className="container">
+          <Link to="/" className="form__link">
+            Go Back
+          </Link>
+          <form className="AddContact__form" onSubmit={this.addContact}>
+            <div className="form__control">
+              <label htmlFor="name">Name: </label>
+              <input
+                type="text"
+                id="name"
+                onChange={this.onChangeName}
+                required
+              />
+            </div>
+            <div className="form__control">
+              <label htmlFor="phone">Phone: </label>
+              <input
+                type="text"
+                id="phone"
+                onChange={this.onChangePhone}
+                required
+              />
+            </div>
+            <h5 className="form__error">{errorMessage}</h5>
+            <hr />
+            <h5 className="text-info">Subscriber to be added:</h5>
+            <div className="form__display">
+				<b>Name: <span>{this.state.name}</span></b>
+            </div>
+            <div className="form__display">
+				<b>Phone: <span>{this.state.phone}</span></b>
+            </div>
+            <button type="submit" className="btn btn-success">
+              ADD
+            </button>
+          </form>
+        </div>
+      </Fragment>
     );
   }
 }
 
-export default App;
+class Main extends Component {
+	render() {
+		return (
+			  <div className="Main">
+				<Header>PHONE DIRECTORY</Header>
+				<div className="Main__container">
+				  <Link to="/add" className="btn btn-success">
+					ADD
+				  </Link>
+				  <Contacts
+					list={this.props.contacts}
+					onDelete={this.props.deleteContact}
+				  />
+				</div>
+			  </div>
+			);
+	}
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    };
+  }
+  addContact = contact => {
+    const { name, phone } = contact;
+    const newContacts = [...this.state.contacts];
+    newContacts.push({ name, phone, id: newContacts.length + 1 });
+    this.setState(
+      {
+        contacts: newContacts
+      },
+      () => {
+        this.props.history.push("/");
+      }
+    );
+  };
+  deleteContact = id => {
+    const newContacts = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
+    this.setState({
+      contacts: newContacts
+    });
+  };
+  render() {
+    return (
+      <Switch>
+        <Route
+          path="/add"
+          render={() => <AddContact addContact={this.addContact} />}
+        />
+        <Route
+          path="/"
+          render={() => (
+            <Main
+              contacts={this.state.contacts}
+              deleteContact={this.deleteContact}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
+}
+
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+import 'jquery/dist/jquery.min.js';
+import 'jquery/dist/jquery.slim.min.js';
+import 'tether/dist/js/tether.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<BrowserRouter> 
+	<div>
+		<App />
+	</div>
+  </BrowserRouter>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
